7f6da793e5b46a9882eb3a3e22cecdf9643540a5
pech <107599018+Jimsy1977@users.noreply.github.com>
2022-10-03 22:30:44 -0500

    primera edicion

---
+++ data/CT_4.js
@@ -0,0 +1,8 @@
+var json_CT_4 = {
+"type": "FeatureCollection",
+"name": "CT_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Nombre": "SET SAN JOSE", "Pot_Inst": "0.32 MVA", "Rel_Transf": "34.5 / 10 KV" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -78.736085380818778, -8.449991150345411 ], [ -78.736013162764422, -8.450027149626267 ], [ -78.736013162764422, -8.450027149626267 ], [ -78.736040268105242, -8.450083069957111 ], [ -78.736112502769487, -8.450044225690494 ], [ -78.736085380818778, -8.449991150345411 ] ] ] ] } }
+]
+}
